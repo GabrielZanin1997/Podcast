1dda3f7fbc05e0e5e8b525e4f5523e3cc9e511c1
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2021-04-23 13:29:41 -0300

    dev Player of the podcast

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,18 +3,20 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import { api } from '../../services/api';
+import Head from 'next/head';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import Link from 'next/link';
 import Image from 'next/image';
 
 import styles from './episode.module.scss';
+import { usePlayer } from '../../contexts/PlayerContext';
 
 type Episode = {
   id: string;
   title: string;
   thumbnail: string;
   members: string;
-  duration: string;
+  duration: number;
   durationAsString: string;
   url: string;
   publishedAt: string;
@@ -26,9 +28,13 @@ type EpisodeProps = {
 };
 
 export default function Epside({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
  
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title}</title>
+      </Head>
         <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
@@ -41,7 +47,7 @@ export default function Epside({ episode }: EpisodeProps) {
             src={episode.thumbnail}
             objectFit="cover"
           />
-          <button type="button">
+          <button type="button" onClick={() => play(episode)}>
             <img src="/play.svg" alt="Tocar episódio" />
           </button>
         </div>
